0f7305c0d6f69bf741c6f4d923a292d87ff80a51
NguyenThanhCo2706 <conguyen.270601@gmail.com>
2022-11-28 00:12:46 +0700

    feat: implement api get list users

---
+++ DUTPS.API/Services/UserService.cs
@@ -0,0 +1,46 @@
+using DUTPS.API.Dtos.Commons;
+using DUTPS.API.Dtos.Profile;
+using DUTPS.API.Dtos.Vehicals;
+using DUTPS.Databases;
+using Microsoft.EntityFrameworkCore;
+
+namespace DUTPS.API.Services
+{
+  public interface IUserService
+  {
+    Task<List<ProfileDto>> GetUsers();
+  }
+  public class UserService : IUserService
+  {
+    private readonly DataContext _context;
+
+    public UserService(DataContext context)
+    {
+      _context = context;
+    }
+
+    public async Task<List<ProfileDto>> GetUsers()
+    {
+      return await _context.Users.Select(x => new ProfileDto
+      {
+        Id = x.Information.UserId,
+        Username = x.Username,
+        Email = x.Email,
+        Role = x.Role,
+        Name = x.Information.Name,
+        Gender = x.Information.Gender,
+        Birthday = x.Information.Birthday,
+        PhoneNumber = x.Information.PhoneNumber,
+        Class = x.Information.Class,
+        FacultyId = x.Information.FacultyId,
+        FalcultyName = x.Information.Faculty.Name,
+        Vehicals = x.Vehicals.Select(y => new VehicalDto
+        {
+          Id = y.Id,
+          LicensePlate = y.LicensePlate,
+          Description = y.Description
+        }).ToList()
+      }).ToListAsync();
+    }
+  }
+}
